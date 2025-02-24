88b4fbe3a084c167b8c0aa36e7ee16d116137028
Adrian Rivera <adrianluna0830@gmail.com>
2025-02-23 19:07:18 -0600

    Add buffer struct

--- Packet.h
+++ Packet.h
@@ -4,6 +4,25 @@
 
 #ifndef PACKET_H
 #define PACKET_H
+#include <cstdint>
+
+struct Buffer {
+    uint8_t *m_buffer;
+    int index;
+    int m_size;
+
+    Buffer(int size) : m_size(size), index(0) {
+        m_buffer = new uint8_t[size]();
+    }
+
+    ~Buffer() {
+        delete[] m_buffer;
+    }
+
+    Buffer(const Buffer&) = delete;
+    Buffer& operator=(const Buffer&) = delete;
+
+};
 
 struct PacketHeader {
 
